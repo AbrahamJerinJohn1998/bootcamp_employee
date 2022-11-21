74ec4fe2a5f01d7083bcedb312a591d6f46379c6
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-11-21 15:19:04 +0530

    Input reading

---
+++ employee.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class employee {
+    public static void main(String[] args) {
+        int choice;
+        Scanner sc=new Scanner(System.in);
+        ArrayList arr= new ArrayList<>();
+        System.out.println("Enter 1-Add employee details\n 2-View employee details\n3-Search employee details\n4-Delete employee details\n5-Exit\nEnter your choice: ");
+        choice= sc.nextInt();
+    }
+}
